f3953e6152c6760526f335604594dd054f43c1ce
Ashi Krishnan <queerviolet@github.com>
2021-02-04 13:17:23 -0500

    Add glossary.

--- bin/render.js
+++ bin/render.js
@@ -1,26 +1,27 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
+const {writeFileSync, fstat} = require('fs')
 const {resolve} = require('path')
- 
+
+const log = (...args) => console.error(...args)
+const writeHtml = (filename, html) =>
+  console.log(`<!doctype html>\n${html}`)
+  // writeFileSync(filename, '<!doctype html>\n' + html, 'utf8')
+
 async function main() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  page.on('console', msg => console.log(msg.text()))
-  page.on('pageerror', error => {
-    console.log(error.message);
-   });
-  page.on('response', response => {
-    console.log(response.status(), response.url());
-  });
-  page.on('requestfailed', request => {
-    console.log(request.failure().errorText, request.url());
-  });  
+  page.on('console', msg => log(msg.text()))
+  page.on('pageerror', error => log)
+  page.on('response', response => log(response.status(), response.url()))
+  page.on('requestfailed', request =>
+    log(request.failure().errorText, request.url()))
 
   const [_node, _main, url] = process.argv
-  console.log('Loading', url)
+  log('Loading', url)
   await page.goto(url)
-  console.log(url, 'loaded')
+  log(url, 'loaded')
 
-  console.log('Waiting for page to finish rendering...')
+  log('Waiting for page to finish rendering...')
   const html = await page.evaluate(() => new Promise(resolve => {
     if (window.__didFinishRendering) return resolveText()
     window.__didFinishRendering = resolveText
@@ -32,9 +33,8 @@ async function main() {
       resolve(document.documentElement.outerHTML)
     }
   }))
-  console.log('Content rendered.')
-
-  require('fs').writeFileSync('./rendered.html', '<!doctype html>\n' + html, 'utf8')
+  log('Content rendered.')
+  writeHtml('rendered.html', html)
  
   await browser.close();
 }
