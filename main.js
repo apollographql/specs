1c00b6cbc288d234960c16fab45b99538fda7524
hwillson <hugh@octonary.com>
2021-03-31 10:40:21 -0400

    Remove no longer needed theming code
    Since we're aligning the visual look with the look our main
    docs use, we don't need to support multiple themes.

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-import {styles} from './load.js'
-import {result, sleep} from './async.js'
 import markdown from './markdown.js'
-import highlight from './highlight.js'
-import nav from './nav.js'
 import toc from './toc.js'
 import {go} from './rendering.js'
 
@@ -11,39 +7,11 @@ addEventListener('load', main)
 async function main() {
   removeEventListener('load', main)
 
-  const theme = setTheme('dark')
-
   if (!document.documentElement.dataset.rendered) {
-    const mermaid = import('./mermaid.js')
     go(() => Promise.all([
       markdown(),
-      nav(),
-      highlight(),
-      result(theme).then(() => mermaid)
-        .then(mermaid => mermaid.default()),
     ]))
   }
 
   toc()
-}
-
-async function setTheme(name) {
-  let failed = null
-  styles(`/${name}.css`).catch(err => failed = err)
-
-  const loaded = () =>
-    getComputedStyle(document.documentElement)
-      .getPropertyValue('--theme').trim() === name
-
-  while (!loaded()) {
-    console.log('theme not loaded, waiting...')
-    await sleep(100)
-    if (failed) {
-      console.error(failed)
-      break
-    }
-  }
-  
-  const curtain = document.getElementById('__curtain__')
-  curtain && curtain.remove()
 }
